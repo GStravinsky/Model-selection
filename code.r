7b7e5d49deea74b9a4f32ca900a102c0fd9c09c1
GStravinsky <52568826+GStravinsky@users.noreply.github.com>
2019-11-19 16:35:57 +0000

    Update code.r

--- code.r
+++ code.r
@@ -18,7 +18,8 @@ str(gold)
 attach(gold)
 pairs(age~., data=gold[,c(2:5)])
 # curious about the zero values.....  they mean something
-# but only  cash withrawals and cash credits seem correlated
+# but only  cash withrawals and cash credits seem correlated and to some 
+# extent cash widtdrawals and credit withdrawals. 
 par(mfrow=c(2,2))
 hist(age)
 hist(mcardwdl)
@@ -32,6 +33,7 @@ hist(mcashcr[mcashcr!=0])
 hist(mcashwd[mcashwd!=0])
 # Not even close to normal but oh well
 cor(gold[,c(2:5)])
+# same conclussion as the pair plot. 
 
 barplot(prop.table(table(second)))
 barplot(prop.table(table(sex)))
@@ -39,9 +41,24 @@ barplot(prop.table(table(frequency)))
 barplot(prop.table(table(carduse)))
 barplot(prop.table(table(type)))
 
+length(mcardwdl[mcardwdl==0])
+length(carduse[carduse=="No"])
+# perfect explanation
 
 
+############# Part 2 ##########
+# turn off scientific display of numbers
+options(scipen = 999)
+options(digits=4)
 
+a <- glm(Gold~age*carduse+., family = binomial, data = gold)
+summary(a)
+# age, carduseno, credit withdrawals, sex, interaction
 
-
-pairs(~age+mcardwdl+mcashcr+mcashwd+sex)
+#### 2C
+step(a, direction = "backward", test = "F")
+# - frequency, -mcashcr. - second. - type
+# leave mcardwdl, mcashwd, interaction, sex, age, carduse. AIC = 891
+# a bit different from a suggestion of simple individual significance
+a1 <- glm(Gold~age*carduse+ mcardwdl + mcashwd + sex, family = binomial, data = gold)
+summary(a1)
